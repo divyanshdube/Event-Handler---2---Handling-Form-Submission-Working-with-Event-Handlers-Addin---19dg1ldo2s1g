f3dfe8037098df40aab56ea1fe849460b2537a42
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-10-01 12:28:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,27 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 const App = () => {
+  const handleClick = (event) => {
+    event.target.value;
+    console.log("Button id is:-button-a");
+  };
 
-  const handleClick = (event) =>{
-    // use console.log
-
-  }
+  const handleClick2 = (event) => {
+    event.target.value;
+    console.log("Button id is:-button-b");
+  };
 
   // do not remove the two buttons or change their id
   return (
     <div id="main">
-      <button id="button-a">Button A</button>
-      <button id="button-b">Button B</button>
+      <button id="button-a" onClick={handleClick}>
+        Button A
+      </button>
+      <button id="button-b" onClick={handleClick2}>
+        Button B
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
